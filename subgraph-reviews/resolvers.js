0625789d76e767e8543ee9a4ea2ba6573f0ac521
duyvu997 <ngocduy0997@gmail.com>
2023-04-12 08:31:49 +0700

    add router

--- subgraph-reviews/resolvers.js
+++ subgraph-reviews/resolvers.js
@@ -1,31 +1,37 @@
 const resolvers = {
   Query: {
-    latestReviews: (_, __, {dataSources}) => {
+    latestReviews: (_, __, { dataSources }) => {
       return dataSources.reviewsAPI.getLatestReviews();
-    }
+    },
   },
   Mutation: {
-    submitReview: (_, {locationReview}, {dataSources}) => {
-      const newReview = dataSources.reviewsAPI.submitReviewForLocation(locationReview);
-      return {code: 200, success: true, message: 'success', locationReview: newReview};
-    }
+    submitReview: (_, { locationReview }, { dataSources }) => {
+      const newReview =
+        dataSources.reviewsAPI.submitReviewForLocation(locationReview);
+      return {
+        code: 200,
+        success: true,
+        message: 'success',
+        locationReview: newReview,
+      };
+    },
   },
   Review: {
-    location: ({locationId}) => {
-      return {id: locationId};
+    location: ({ locationId }) => {
+      return { id: locationId };
     },
   },
   Location: {
     __resolveReference: (referencedLocation) => {
-      return referencedLocation
-   },
+      return referencedLocation;
+    },
     overallRating: ({ id }, _, { dataSources }) => {
       return dataSources.reviewsAPI.getOverallRatingForLocation(id);
     },
     reviewsForLocation: ({ id }, _, { dataSources }) => {
       return dataSources.reviewsAPI.getReviewsForLocation(id);
     },
-  }
+  },
 };
 
 module.exports = resolvers;
